852f79eec6e5811d61f0428815b079412ace5386
Richard Cornish <richardcornish@gmail.com>
2017-07-17 14:08:34 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='django-removewww',
-    version='0.1.1',
+    version='0.1.2',
     description='A Django middleware application that removes the WWW subdomain',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Richard Cornish',
